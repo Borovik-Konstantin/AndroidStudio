2ec9615e4039e931c76e7f473d4cef92052fe2bf
Kostik <QKostikQ@yandex.ru>
2023-04-20 16:54:31 +0300

    Lab 9. Connection frontend and backend. Login and logout in localhost:3000/login

--- BackEnd/front/src/App.js
+++ BackEnd/front/src/App.js
@@ -38,6 +38,7 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
+import Login from "./components/Login"
 
 function App() {
     return (
@@ -47,8 +48,10 @@ function App() {
                 <div className="container-fluid">
                     <Routes>
                         <Route path="home" element={<Home />}/>
+
                     </Routes>
                 </div>
+                <Login/>
             </BrowserRouter>
         </div>
     );
